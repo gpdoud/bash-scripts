7db7e501e43345d30e0f773092241d068bccc2ee
Greg Doud <gpdoud@gmail.com>
2019-03-29 12:16:06 -0400

    --amend

--- check-git.sh
+++ check-git.sh
@@ -3,27 +3,31 @@
 # whether git repositories are clean and whether
 # they have a remote defined
 #
-dirtyfiles=/c/repos/git.dirty.files
-noremote=/c/repos/github.remote.exists
+TMP=/tmp
+DIRTY_FILES="$TMP/git.dirty.files"
+NO_REMOTE="$TMP/github.remote.exists"
+
 for gitpath in $(find . -iname ".git"); do
     [ -d "$gitpath" ] || continue # if not a dir, continue
     (
         cd "$gitpath" && cd ..
         cdir=`pwd`
-        git status -s > "$dirtyfiles"
+        git status -s > "$DIRTY_FILES"
         clean=""
-        if [[ -s "$dirtyfiles" ]]; then
+        if [[ -s "$DIRTY_FILES" ]]; then
             clean="not clean"
         fi
-        rm "$dirtyfiles"
+        rm "$DIRTY_FILES"
         # check if GitHub connection exists
         remote=""
-        git remote -v > "$noremote"
-        if [[ ! -s "$noremote" ]]; then
+        git remote -v > "$NO_REMOTE"
+        if [[ ! -s "$NO_REMOTE" ]]; then
             remote="No remote"
         fi
-        rm "$noremote"
-        echo "$cdir - $clean $remote"
+        rm "$NO_REMOTE"
+        if [[ ! "$clean$remote" == "" ]]; then
+            echo "$cdir - $clean $remote"
+        fi
     )
     # git --git-dir=x/.git --work-tree=x status
 done
