13c6cd8759fcaaf4a4033a8ef93171b596306e01
Greg Doud <gpdoud@gmail.com>
2019-03-29 11:56:07 -0400

    --amend

--- check-git.sh
+++ check-git.sh
@@ -1,32 +1,29 @@
-# if [ -d ./angular ]; then
-#     echo "Yes Angular Directory"
-# fi
-# if [ -d ./angular/test-cors/.git ]; then
-#     echo "Yes Angular/test-cores/.git Directory"
-# fi
+#
+# scans the current and all subfolders and reports
+# whether git repositories are clean and whether
+# they have a remote defined
+#
 dirtyfiles=/c/repos/git.dirty.files
 noremote=/c/repos/github.remote.exists
 for gitpath in $(find . -iname ".git"); do
-    [ -d "$gitpath" ] || continue
-    if [ -d "$gitpath" ]; then
-        (
-            cd "$gitpath" && cd ..
-            cdir=`pwd`
-            git status -s > "$dirtyfiles"
-            clean=""
-            if [[ -s "$dirtyfiles" ]]; then
-                clean="not clean"
-            fi
-            rm "$dirtyfiles"
-            # check if GitHub connection exists
-            remote=""
-            git remote -v > "$noremote"
-            if [[ ! -s "$noremote" ]]; then
-                remote="No remote"
-            fi
-            rm "$noremote"
-            echo "$cdir - $clean $remote"
-        )
-    fi
+    [ -d "$gitpath" ] || continue # if not a dir, continue
+    (
+        cd "$gitpath" && cd ..
+        cdir=`pwd`
+        git status -s > "$dirtyfiles"
+        clean=""
+        if [[ -s "$dirtyfiles" ]]; then
+            clean="not clean"
+        fi
+        rm "$dirtyfiles"
+        # check if GitHub connection exists
+        remote=""
+        git remote -v > "$noremote"
+        if [[ ! -s "$noremote" ]]; then
+            remote="No remote"
+        fi
+        rm "$noremote"
+        echo "$cdir - $clean $remote"
+    )
     # git --git-dir=x/.git --work-tree=x status
 done
